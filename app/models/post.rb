b97b53ada386f77c5f6220504b8cf73212e623d5
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 05:57:00 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-
+include ActiveModel::Validations
+ PostTitleValidator
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
