7e5ee50a9f0b60fa1379926c100bedaa882bb2ab
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 05:18:39 +0000

    Automatically backed up by Learn

--- app/validators/post_title_validator.rb
+++ app/validators/post_title_validator.rb
@@ -1,6 +1,6 @@
 class PostTitleValidator
   def validate(record)
-    unless.record.match?( "Won't Believe", "Secret", "Top [number]", or "Guess", )
+    unless.record.match?(/"Won't Believe" "Secret" "Top [number]" "Guess"/ )
   end
 
 end
