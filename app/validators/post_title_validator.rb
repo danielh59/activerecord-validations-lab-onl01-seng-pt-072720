db0e04c963da215ab7b57d81872f6184fda1dcb2
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 06:03:14 +0000

    Automatically backed up by Learn

--- app/validators/post_title_validator.rb
+++ app/validators/post_title_validator.rb
@@ -1,7 +1,7 @@
 class PostTitleValidator < ActiveModel::Validator
   def validate(record)
     unless record.title.match?(/"Won't Believe"/ )
-      record.errors[:title] << "Not clickbait-y!" 
+      record.errors[:title] << "Not clickbait-y!"
   end
 end
 
