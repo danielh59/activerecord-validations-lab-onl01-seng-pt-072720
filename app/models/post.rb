e1a376f9c12321b63fd757aa04505b7d573fba4b
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 05:05:02 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ActiveRecord::Base
+  a = "Won't Believe" 
+  b = "Secret"
+  c =  "Top [number]" 
+  d =  "Guess" 
   validates :title, presence: true
   validates :count, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
+  validates :title, inclusion: { in: %w(small medium large),
+   message: "%{value} is not a valid size" }
 end
