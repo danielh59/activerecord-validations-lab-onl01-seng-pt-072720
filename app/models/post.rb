c88db2eb1c2d38e475f82d6fb88f76dfb0f8b46e
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 05:05:37 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ActiveRecord::Base
-  a = "Won't Believe"
-  b = "Secret"
-  c =  "Top [number]"
-  d =  "Guess"
+  @a = "Won't Believe"
+  @b = "Secret"
+  @c =  "Top [number]"
+  @d =  "Guess"
   validates :title, presence: true
   validates :count, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :title, inclusion: { in: %w(a b c d),
+  validates :title, inclusion: { in: %w(@a @b @c @d),
    message: "%{value} is not a valid size" }
 end
