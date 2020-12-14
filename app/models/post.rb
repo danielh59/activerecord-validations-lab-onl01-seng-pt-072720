b4f3ac2a68457d39566861cac8618673bf837bad
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 05:10:06 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :count, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  a = "Won't Believe"
-  b = "Secret"
-  c =  "Top [number]"
-  d =  "Guess"
-  validates :title, inclusion: { in: %w(a b c d),
-   message: "Not good enough" }
+
+
 end
