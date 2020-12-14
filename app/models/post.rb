4dc3b47abeda3c52eb662ec9536e3eb231ac8f8d
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 05:02:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :count, length: { minimum: 250 }
-  validates :summar, length: { maximum: 250 }
+  validates :summary, length: { maximum: 250 }
 end
